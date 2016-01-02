189e4c2afb5fe875d3e69011896c53051078afb7
David Björklund <david.bjorklund@gmail.com>
2016-01-02 03:47:15 +0100

    cleanup

--- index.js
+++ index.js
@@ -16,50 +16,45 @@ const db = setupDb(path.join(storage, '.sinopia-follow.json'));
 
 const skimdb = 'https://skimdb.npmjs.com/registry';
 const registry = 'https://registry.npmjs.org';
-let p;
+let promise = Promise.resolve();
 
 db.open()
   .then(() => { return db.get('npmjs'); })
   .then((since = 'now') => {
     return new Promise((resolve, reject) => {
-      follow({ db: skimdb, since: since.toString() }, (err, data) => {
+      follow({ db: skimdb, since: since.toString() }, (err, { id, seq }) => {
         if (err) {
           return reject(err);
         }
-        p = Promise.resolve(p)
-          .then(() => { return data; })
-          .then(handleData);
+
+        if (exists(id)) {
+          promise = promise
+            .then(() => { return { id }; })
+            .then(syncWithRegistry);
+        }
+
+        promise = promise.then(() => {
+          console.log('Sync', seq, id);
+
+          return db.put('npmjs', String(seq));
+        });
       });
     });
   })
   .catch((err) => { throw err; });
 
-function handleData ({ id, seq }) {
-  console.log('Sync', seq, id);
-  return new Promise((resolve, reject) => {
-    if (!exists(id)) {
-      return resolve();
-    }
-
-    return fetch(registry + '/' + id)
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        fs.writeFileSync(
-          path.join(storage, id, 'package.json'),
-          JSON.stringify(json, null, '\t')
-        );
-        console.log(id, 'updated');
-        resolve();
-      });
-  })
-  .then(() => {
-    return db.put('npmjs', String(seq));
-  })
-  .catch(function (err) {
-    throw err;
-  });
+function syncWithRegistry ({ id }) {
+  return fetch(registry + '/' + id)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      fs.writeFileSync(
+        path.join(storage, id, 'package.json'),
+        JSON.stringify(json, null, '\t')
+      );
+      console.log(id, 'updated');
+    });
 }
 
 function exists (name) {
