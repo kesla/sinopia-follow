68d05bec36cb028613482acb24215ea3247d32d7
David Björklund <david.bjorklund@gmail.com>
2016-01-02 12:34:53 +0100

    parse args

--- cli.js
+++ cli.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 
+process.title = 'sinopia-follow';
+
 require('babel-core/register')();
-require('./');
+var start = require('./').default;
+var args = require('./lib/args').default();
+
+if (args.configPath) {
+  start(args)
+    .catch(function (err) {
+      console.log(err.stack);
+    });
+}
--- index.js
+++ index.js
@@ -6,36 +6,29 @@ import path from 'path';
 import follow from './lib/follow';
 import setupDb from './lib/db';
 import syncWithRegistry from './lib/sync-with-registry';
+import assert from 'assert';
 
 const skimdb = 'https://skimdb.npmjs.com/registry';
 const registry = 'https://registry.npmjs.org';
 
-(async function () {
-  // make this configurable
-  const config = YAML.safeLoad(await fs.readFile(
-    path.join(process.env.HOME, '.config/sinopia/config.yaml')
-  ));
+export default async function ({ configPath }) {
+  assert(configPath, 'required: configPath');
+
+  const config = YAML.safeLoad(await fs.readFile(configPath));
   const { storage } = config;
   const db = setupDb(path.join(storage, '.sinopia-follow.json'));
 
-  try {
-    await db.open();
-    const since = await db.get('npmjs') || 'now';
-    const getChange = follow({ db: skimdb, since: since.toString() });
+  await db.open();
+  const since = await db.get('npmjs') || 'now';
+  const getChange = follow({ db: skimdb, since: since.toString() });
 
-    while (true) {
-      let { id, seq } = await getChange();
+  while (true) {
+    let { id, seq } = await getChange();
 
-      if (await fs.exists(path.join(storage, id))) {
-        await syncWithRegistry({ id, config, registry });
-      }
-      await db.put('npmjs', String(seq));
-      console.log('Sync', seq, id);
+    if (await fs.exists(path.join(storage, id))) {
+      await syncWithRegistry({ id, config, registry });
     }
-  } catch (err) {
-    if (err) {
-      console.log(err);
-      console.log(err.stack);
-    }
+    await db.put('npmjs', String(seq));
+    console.log('Sync', seq, id);
   }
-})();
+};
